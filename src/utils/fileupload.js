e8112c14290685c5a4a291f01a17825f6d1df5b5
Sumit639 <sumitkkumar639@gmail.com>
2024-04-20 22:25:11 +0530

    file upload work

---
+++ src/utils/fileupload.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+//fs is a file system from node js
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const upload = async (local) => {
+  try {
+    if (!local) return null;
+    const res = await cloudinary.uploader.upload(local, {
+      resource_type: "auto",
+    });
+    //file is been uploaded
+    console.log("uploaded on cloudinary", res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(local);
+    //remove the local file that has been saved in local storage
+    return null;
+  }
+};
