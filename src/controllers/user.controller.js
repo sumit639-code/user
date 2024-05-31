1b73db4d7735a81c807a15775c9d8a87420342f1
Sumit639 <sumitkkumar639@gmail.com>
2024-05-31 11:00:50 +0530

    fixed refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -152,7 +152,7 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(req.user._id, {
     $set: {
-      refreshToken: undefined,
+      refreshToken: "",
     },
   });
   const options = {
